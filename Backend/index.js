03cf1a5e9dba773b409f360a31b06ea84e261fde
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2024-03-24 16:15:27 +0530

    backend work has been started

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { readFile } = require('fs');
+const app = express();
+
+app.get('/', (request, response) =>{
+
+    readFile('../Frontend/create_new_account.html','utf8', (err,html)=>{
+        if(err){
+            response.status(500).send('sorry, out of order')
+        }
+
+        response.send(html);
+    })
+})
+
+app.listen(process.env.PORT || 3000,  ()=>console.log('App avalaible on port 3000'))
