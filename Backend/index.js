72248b7bf695fd0c8108490f111d1123e5d89a09
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2024-03-24 21:52:06 +0530

    added the comment I needed to do

--- Backend/index.js
+++ Backend/index.js
@@ -16,3 +16,7 @@ app.get('/', (request, response) =>{
 app.listen(process.env.PORT || 3000,  ()=>console.log('App avalaible on port 3000'))
 
 
+//1. connect the database
+//2. create the user table when users signup
+//3. In the login section fetch the data from the table and check and do the validation
+//4. show the login succesfull and the profile picture
