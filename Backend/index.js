49a4bfbce2973b560633f869a862163be74a472a
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2024-03-25 21:04:49 +0530

    adding nodemon package to our app

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { readFile } = require('fs');
 const app = express();
+var mysql = require('mysql');
 
 //Making a server connection
 app.get('/', (request, response) =>{
@@ -22,21 +23,21 @@ app.listen(process.env.PORT || 3000,  ()=>console.log('App avalaible on port 300
 //3. In the login section fetch the data from the table and check and do the validation
 //4. show the login succesfull and the profile picture
 
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "educhatroom"
-});
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO users (email, username, password) VALUES ()";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-});
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "educhatroom"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql = "INSERT INTO users (email, username, password) VALUES ()";
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log("1 record inserted");
+//   });
+// });
